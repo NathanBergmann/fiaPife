49ac65c6e97e3a4283e44a0cb519c5db187363a2
luizakohn <luizakohn21@gmail.com>
2024-03-27 20:29:31 -0300

    Modelado a Classe Cards
    Criados os atributos, getter e setter e construtor da classe

--- backend/src/Entities/Cards.java
+++ backend/src/Entities/Cards.java
@@ -1,5 +1,36 @@
 package Entities;
 
 public class Cards {
+
+    private String naipe;
+    private String numero;
+    private boolean isDisponivel;
+
     
+    public Cards(String naipe, String numero) {
+        setDisponivel(true);
+        setNumero(numero);
+        setNaipe(naipe);
+    }
+
+    public String getNaipe() {
+        return naipe;
+    }
+    public void setNaipe(String naipe) {
+        this.naipe = naipe;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public boolean isDisponivel() {
+        return isDisponivel;
+    }
+    public void setDisponivel(boolean isDisponivel) {
+        this.isDisponivel = isDisponivel;
+    }
+
+
 }
