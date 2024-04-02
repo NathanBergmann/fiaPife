2d9f1acbb53cd9a9b32912f40c2b4a0eb649fc39
NathanBergmann <nathan.bergmann@agapys.com>
2024-04-02 20:03:00 -0300

    Adicionado Classe Player
    * Added class player with his methods

---
+++ backend/src/Entities/Player.java
@@ -0,0 +1,42 @@
+package Entities;
+
+public class Player {
+    private String name;
+    private int wins = 0;
+    private Cards[] cards = new Cards[10];
+    
+    public Player(String name){
+        setName(name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addWins() {
+        this.wins = this.wins +1;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+    
+    public void setNewCard(){
+    }
+
+    public void getLastCardPlayed(){
+
+    }
+
+    public void calculateCombinations(){
+
+    }
+
+    public Cards[] getCards() {
+        return cards;
+    }
+}
