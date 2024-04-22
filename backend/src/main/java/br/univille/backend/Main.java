8af0aaa4dde43f66d6a0712b2dc3359ec1b518f3
NathanBergmann <nathan.bergmann@agapys.com>
2024-04-21 22:15:12 -0300

    Add Sortear Cartas
    * Adicionado logica que valida se a carta está disponível;
    * Adicionado logica que busca a carta que está disponível.

--- backend/src/main/java/br/univille/backend/Main.java
+++ backend/src/main/java/br/univille/backend/Main.java
@@ -3,20 +3,28 @@ package br.univille.backend;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.univille.backend.Entities.*;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Main {
+	private static String namePlayer;
 
 	public static void main(String[] args){
+		/*Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Informe o nome do Primeiro Jogador: ");
+		namePlayer = scanner.nextLine();
+		Player player1 = new Player(namePlayer);
+		Player iaPlayer = new Player("IA");
+		*/		
 		Cards carta;
 		Deck deck = new Deck();
 		
-		deck.deckLaunch();;
-		
-
-		carta = deck.cardsDistribute(13);
-		System.out.println(carta.toString());
-
+		deck.deckLaunch();
+		for(int i=0; i< 10;){
+			System.out.println(deck.deliveCards());
+			i ++;
+		}
 	}
 
 }
